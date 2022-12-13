e9a4f4170f03f1fb830bed6be51392e2e80f510a
Blaise Antonio I <antonio.iradukunda@gmail.com>
2022-12-13 13:50:24 +0100

    Debug

--- test.php
+++ test.php
@@ -2,10 +2,10 @@
 
     require_once 'MathExpr.php';
 
-    $ex1 = "3.5+(3*6+(2*2))";
+    $ex1 = "6+-(4)";
     $ex2 = "3*2+5";
     $ex3 = "1-(-5)";
-    $expr = new MathExpr($ex1);
+    $expr = new MathExpr($ex2);
     $result = $expr->evaluate();
     echo $expr->getExpression()." = $result \n";
 ?>
